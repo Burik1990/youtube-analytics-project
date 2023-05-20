48b19af7f2ac8cdd70c5e1811f130d32aa4dcd4a
Burik1990 <bury1990@mail.ru>
2023-05-20 20:05:31 +0300

    Модифицируем конструктор `Channel`...Добавили методы get_service и to_json, сохраняющий в файл значения атрибутов

--- src/channel.py
+++ src/channel.py
@@ -9,11 +9,36 @@ class Channel:
     api_key: str = os.getenv('API_KEY')
     youtube = build('youtube', 'v3', developerKey=api_key)
 
+    @classmethod
+    def get_service(cls):
+        '''Возвращает объект для работы с YouTube API'''
+        return cls.youtube
+
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.__channel_id = channel_id
         self.channel = Channel.youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+        self.title = self.channel['items'][0]['snippet']['title']
+        self.description = self.channel['items'][0]['snippet']['description']
+        self.url = 'https://www.youtube.com/channel/' + self.channel['items'][0]['id']
+        self.subscriber_count = self.channel['items'][0]['statistics']['subscriberCount']
+        self.video_count = self.channel['items'][0]['statistics']['videoCount']
+        self.view_count = self.channel['items'][0]['statistics']['viewCount']
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         print(json.dumps(self.channel, indent=2, ensure_ascii=False))
+
+    def to_json(self, file):
+        '''сохраняет в файл значения атрибутов экземпляра `Channel`'''
+        data = {
+            'ID' : self.__channel_id,
+            'Name' : self.title,
+            'Description' : self.description,
+            'Url' : self.url,
+            'Subscriber count' : self.subscriber_count,
+            'Video count' : self.video_count,
+            'View count' : self.view_count
+        }
+        with open(file, 'w', encoding='utf-8') as info_file:
+            info_file.write(json.dumps(data, ensure_ascii=False))
