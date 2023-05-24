cf5589eb3cd39b1fe830b3bf7ec778a92f5a4ddd
Burik1990 <bury1990@mail.ru>
2023-05-24 23:33:33 +0300

    dz4 Создали классы Video и PLVideo в файле src/video

---
+++ src/video.py
@@ -0,0 +1,28 @@
+import os
+from googleapiclient.discovery import build
+
+
+class Video:
+    """Класс для представления видео"""
+    api_key: str = os.getenv('API_KEY')
+    youtube = build('youtube', 'v3', developerKey=api_key)
+
+    def __init__(self, video_id):
+
+        self.video_id = video_id
+        self.video_response = Video.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                          id=video_id).execute()
+        self.title = self.video_response['items'][0]['snippet']['title']
+        self.url = 'https://youtu.be/gaoc9MPZ4bw' + video_id
+        self.veuw_count = self.video_response['items'][0]['statistics']['viewCount']
+        self.like_count = self.video_response['items'][0]['statistics']['likeCount']
+
+    def __str__(self):
+        return self.title
+
+
+class PLVideo(Video):
+    """Класс для представления плейлиста"""
+    def __init__(self, video_id, playlist_id):
+        super().__init__(video_id)
+        self.playlist_id = playlist_id
