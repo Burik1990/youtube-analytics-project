13254ce071ad9381c3b2d8f070aca219b969eaec
Burik1990 <bury1990@mail.ru>
2023-05-25 21:36:42 +0300

    dz5 Создали классы PlayList в файле src/playlist.py

---
+++ src/playlist.py
@@ -0,0 +1,53 @@
+import datetime
+import os
+import isodate
+
+from googleapiclient.discovery import build
+from typing import List
+
+from src.video import Video
+
+
+class PlayList:
+    '''Класс для предсавления плейлиста'''
+
+    api_key: str = os.getenv('API_KEY')
+    youtube = build('youtube', 'v3', developerKey=api_key)
+
+    def __init__(self, playlist_id):
+        self.playlist_id = playlist_id
+        self.playlist_videos = PlayList.youtube.playlistItems().list(playlistId=self.playlist_id,
+                                                                     part='contentDetails ,snippet',
+                                                                     maxResults=50,
+                                                                     ).execute()
+        self.title = self.playlist_videos['items'][0]['snippet']['title'].split('.')[0]
+        self.url = 'https://www.youtube.com/playlist?list=' + self.playlist_id
+        self.video_ids: List[str] = [video['contentDetails']['videoId'] for video in self.playlist_videos['items']]
+        self.video_response = Video.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                          id=self.video_ids).execute()
+
+
+    @property
+    def total_duration(self):
+        '''Возвращает объект класса `datetime.timedelta` с суммарной длительность плейлиста'''
+
+        total_duration = datetime.timedelta()
+        for video in self.video_response['items']:
+            # YouTube video duration is in ISO 8601 format
+            iso_8601_duration = video['contentDetails']['duration']
+            video_duration = isodate.parse_duration(iso_8601_duration)
+            total_duration += video_duration
+        return total_duration
+
+    @property
+    def show_best_video(self):
+        '''Возвращает ссылку на самое популярное видео из плейлиста (по количеству лайков)'''
+
+        best_video_id = 0
+        best_like = 0
+        for video in self.video_response['items']:
+            video_id = video['id']
+            like_count = video['statistics']['likeCount']
+            if int(like_count) > best_like:
+                best_video_id = video_id
+        return f'https://youtu.be/{best_video_id}'
